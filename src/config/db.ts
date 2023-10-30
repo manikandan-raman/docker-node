73e49e0a33bcd8bc726b5b052c7b241d82d13a96
Manikandan <37401797+maniyuvan@users.noreply.github.com>
2023-10-30 12:43:15 +0530

    update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,6 +3,13 @@ import { Todo } from '../entities/todo.entity';
 
 export async function connectDB() {
   try {
+    console.log({
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    });
     const dataSource: DataSource = new DataSource({
       type: 'postgres',
       host: process.env.DB_HOST,
