580b54e4af6d34f1f8a0de29b68af426dc9768c0
Manikandan <maniyuvan446@gmail.com>
2023-11-02 11:10:21 +0530

    updated cicd-ec2-pipeline.yaml & disabled syncronize in typeorm config

--- src/config/db.ts
+++ src/config/db.ts
@@ -17,7 +17,7 @@ export async function connectDB() {
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      synchronize: true,
+      synchronize: false,
       logging: false,
       entities: [Todo],
       ssl: {
